7828ae4784427c50f867ba3bffd7c3542d7274f8
Seongbae Chang <dorbae.io@gmail.com>
2018-09-30 17:08:42 +0900

    Initialize project

---
+++ Picture-CleanUp/pic-cleaner-core/src/main/java/io/dorbae/picture/cleanup/core/ExifReader.java
@@ -0,0 +1,17 @@
+package io.dorbae.picture.cleanup.core;
+
+/*****************************************************************
+ * 
+ * ExifReader.java
+ *
+ *****************************************************************
+ *
+ * @version	0.0.0	2018-09-30 17:07:40	dorbae	최초생성
+ * @since 1.0
+ * @author dorbae(dorbae.io@gmail.com)
+ *
+ */
+public class ExifReader {
+
+}
+
